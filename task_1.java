1f73884e6f0d1a8b429028c3f2e8cefa268366d5
Olesya-lia <li5a1909@yandex.ru>
2025-08-12 10:39:18 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,6 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(11);
+	assertEquals("Vfktymrbq eoe",true, isAdult);// Напиши код здесь
+}
